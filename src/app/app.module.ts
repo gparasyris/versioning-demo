7f751ba20c3ddc8a11995c481f0ff17fd789673a
Yorgos Parasyris <38101481+gparasyris@users.noreply.github.com>
2021-11-25 16:12:40 +0000

    new(component): added mock component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoComponentComponent } from './demo-component/demo-component.component';
+import { MockComponentComponent } from './mock-component/mock-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponentComponent
+    DemoComponentComponent,
+    MockComponentComponent
   ],
   imports: [
     BrowserModule,
